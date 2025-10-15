d42235115c652e8618c3d8b09ce8efe56f860aad
matvan99it <vannicraft@gmail.com>
2025-10-15 19:11:02 +0200

    Creazione esercizio 4: Extensions

---
+++ Module2_es2_Extensions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module2_es2_Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
